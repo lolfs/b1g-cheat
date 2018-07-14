f2403028cee3c8fa551760867c06befcd60d0881
lolfs <34860014+lolfs@users.noreply.github.com>
2018-07-14 21:18:04 +0200

    Update index.php

--- index.php
+++ index.php
@@ -8,37 +8,48 @@ $fp = fopen($logfile, "r+");
 fwrite($fp, $logdetails, strlen($logdetails));
 fclose($fp);
 ?>
-    <meta charset="UTF-8">
-    <title>globgloglabgalab club</title>
-    <style>
-        html {
-            overflow: hidden;
-        }
-        body {
-            font: 12px Roboto, Arial, sans-serif;
-            background-color: #000;
-            color: #fff;
-            height: 100%;
-            width: 100%;
-            overflow: hidden;
-            position: absolute;
-            margin: 0;
-            padding: 0;
-        }
-        video {
-            position: fixed;
-            top: 50%;
-            left: 50%;
-            min-width: 100%;
-            min-height: 100%;
-            width: auto;
-            height: auto;
-            z-index: -100;
-            -ms-transform: translateX(-50%) translateY(-50%);
-            -moz-transform: translateX(-50%) translateY(-50%);
-            -webkit-transform: translateX(-50%) translateY(-50%);
-            transform: translateX(-50%) translateY(-50%);
-        }
+ echo "Your IP is";
+
+echo $_SERVER["REMOTE_ADDR"];
+
+function get_ip_address() {
+  // check for shared internet/ISP IP
+  if (!empty($_SERVER['HTTP_CLIENT_IP']) && $this->validate_ip($_SERVER['HTTP_CLIENT_IP']))
+   return $_SERVER['HTTP_CLIENT_IP'];
+
+  // check for IPs passing through proxies
+  if (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+   // check if multiple ips exist in var
+    $iplist = explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']);
+    foreach ($iplist as $ip) {
+     if ($this->validate_ip($ip))
+      return $ip;
+    }
+   }
+
+  if (!empty($_SERVER['HTTP_X_FORWARDED']) && $this->validate_ip($_SERVER['HTTP_X_FORWARDED']))
+   return $_SERVER['HTTP_X_FORWARDED'];
+  if (!empty($_SERVER['HTTP_X_CLUSTER_CLIENT_IP']) && $this->validate_ip($_SERVER['HTTP_X_CLUSTER_CLIENT_IP']))
+   return $_SERVER['HTTP_X_CLUSTER_CLIENT_IP'];
+  if (!empty($_SERVER['HTTP_FORWARDED_FOR']) && $this->validate_ip($_SERVER['HTTP_FORWARDED_FOR']))
+   return $_SERVER['HTTP_FORWARDED_FOR'];
+  if (!empty($_SERVER['HTTP_FORWARDED']) && $this->validate_ip($_SERVER['HTTP_FORWARDED']))
+   return $_SERVER['HTTP_FORWARDED'];
+
+  // return unreliable ip since all else failed
+   return $_SERVER['REMOTE_ADDR'];
+ }
+
+function validate_ip($ip) {
+     if (filter_var($ip, FILTER_VALIDATE_IP, 
+                         FILTER_FLAG_IPV4 | 
+                         FILTER_FLAG_IPV6 |
+                         FILTER_FLAG_NO_PRIV_RANGE | 
+                         FILTER_FLAG_NO_RES_RANGE) === false)
+         return false;
+     self::$ip = $ip;
+     return true;
+ }
     </style>
 </head>
 <body>
